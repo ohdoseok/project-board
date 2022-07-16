45a503203bfa63afaaeefba7f1ed53b44375779b
ohdoseok <pkqwe37@gmail.com>
2022-07-16 21:46:47 +0900

    #8 - 도메인 클래스 설계(게시글, 댓글)

--- project-board/src/main/java/com/example/projectboard/domain/Article.java
+++ project-board/src/main/java/com/example/projectboard/domain/Article.java
@@ -1,2 +1,15 @@
-package com.example.projectboard.domain;public class Article {
+package com.example.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
 }
